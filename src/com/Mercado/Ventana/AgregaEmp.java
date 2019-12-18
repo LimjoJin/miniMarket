d63f963be9e1a2f462692a48899777da673e39a8
Aaron Joel <joe.lim.42.54@gmail.com>
2019-12-17 21:11:54 -0400

    Day 3 Se crearon algunas ventanas mas

--- src/com/Mercado/Ventana/AgregaEmp.java
+++ src/com/Mercado/Ventana/AgregaEmp.java
@@ -1,4 +1,11 @@
 package com.Mercado.Ventana;
 
+import javax.swing.*;
+
 public class AgregaEmp {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton finalizarButton;
 }
